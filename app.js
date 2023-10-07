21a3bbcb1b9ef0f036aa60e46aa7879974671ff5
Petal Ladenson <habonimkef@gmail.com>
2023-10-07 12:58:59 -0600

    pseudocode

---
+++ app.js
@@ -0,0 +1,18 @@
+//game class
+//  constr
+//      calls one of starts
+//      initialize turns
+//      makes sure grid is set up properly?
+//  random start
+//  choice start
+//  hit tile
+//      calls check neighbors
+//      if hit
+//          check num hits
+//      if miss
+//      check turn
+//  check neighbors
+//  game over (win)
+//  game over (out of turns)
+
+//make new game
