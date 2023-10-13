f7840563c12f7a5d30c3f7cc1cecb15fa913a10f
Petal Ladenson <habonimkef@gmail.com>
2023-10-12 18:02:24 -0600

    add modal functionality

--- app.js
+++ app.js
@@ -116,3 +116,21 @@ const openCredits = () => {
   document.getElementById("start").style.display = "none";
   document.getElementById("credits").style.display = "block";
 };
+
+// below is not my code, check credits
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach((trigger) => {
+  trigger.addEventListener("click", (event) => {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", (event) => {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
