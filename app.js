a386d1eecd2ec39e7f1cccb6bdfb7625229f825a
Petal Ladenson <habonimkef@gmail.com>
2023-10-16 19:52:31 -0600

    put cells class in its own file

--- app.js
+++ app.js
@@ -2,15 +2,6 @@ let game;
 const missAudio = new Audio("assets/352103__inspectorj__splash-jumping-g.wav");
 const hitAudio = new Audio("assets/521105__matrixxx__retro-explosion-07.wav");
 const winAudio = new Audio("assets/495005__evretro__win-video-game-sound.wav");
-class Cell {
-  constructor(shipID) {
-    //number
-    this.shipID = shipID;
-
-    //bool
-    this.hit = false;
-  }
-}
 
 //game class
 class Game {
@@ -124,44 +115,6 @@ class Game {
   };
 }
 
-class GameCell extends Cell {
-  constructor(shipID, x, y, game) {
-    super(shipID);
-
-    this.x = x;
-    this.y = y;
-    this.game = game;
-  }
-  setElements = (element, interactable) => {
-    this.element = element;
-    this.interactable = interactable;
-  };
-  hitCell = (e) => {
-    if (this.hit) return;
-    this.hit = true;
-    const hit = this.shipID !== -1;
-    hitAudio.load();
-    missAudio.load();
-    hit ? hitAudio.play() : missAudio.play();
-    this.element.classList.add(hit ? "hit" : "miss");
-    game.hitTile(this.shipID);
-  };
-  showBorder = (up, down, left, right) => {
-    const border = "1vw solid rgba(0,0,0,.4)";
-    if (!left) this.element.style["border-left"] = border;
-    if (!right) this.element.style["border-right"] = border;
-    if (!up) this.element.style["border-top"] = border;
-    if (!down) this.element.style["border-bottom"] = border;
-  };
-  cleanUp = () => {
-    this.element.onclick = "";
-    this.element.innerHTML = "";
-  };
-  hover = () => {
-    this.game.highlight.style.gridRowStart = this.x + 1;
-    this.game.highlight.style.gridColumnStart = this.y + 1;
-  };
-}
 
 //make new game
 const go = (manualStart) => {
---
+++ cells.js
@@ -0,0 +1,57 @@
+class Cell {
+  constructor(shipID) {
+    //number
+    this.shipID = shipID;
+
+    //bool
+    this.hit = false;
+  }
+  hover = () => {
+    this.game.highlight.style.gridRowStart = this.x + 1;
+    this.game.highlight.style.gridColumnStart = this.y + 1;
+  };
+}
+
+class GameCell extends Cell {
+  constructor(shipID, x, y, game) {
+    super(shipID);
+
+    this.x = x;
+    this.y = y;
+    this.game = game;
+  }
+  setElements = (element, interactable) => {
+    this.element = element;
+    this.interactable = interactable;
+  };
+  hitCell = (e) => {
+    if (this.hit) return;
+    this.hit = true;
+    const hit = this.shipID !== -1;
+    hitAudio.load();
+    missAudio.load();
+    hit ? hitAudio.play() : missAudio.play();
+    this.element.classList.add(hit ? "hit" : "miss");
+    game.hitTile(this.shipID);
+  };
+  showBorder = (up, down, left, right) => {
+    const border = "1vw solid rgba(0,0,0,.4)";
+    if (!left) this.element.style["border-left"] = border;
+    if (!right) this.element.style["border-right"] = border;
+    if (!up) this.element.style["border-top"] = border;
+    if (!down) this.element.style["border-bottom"] = border;
+  };
+  cleanUp = () => {
+    this.element.onclick = "";
+    this.element.innerHTML = "";
+  };
+}
+
+class StartCell extends Cell {
+  constructor(x, y, game) {
+    super(-1);
+    this.x = x;
+    this.y = y;
+    this.game = game;
+  }
+}
